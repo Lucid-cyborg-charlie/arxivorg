f4b15b305fe3564ead58c908610cf214c94929c5
Jeremy Auguste <jeremy.auguste@lis-lab.fr>
2020-03-10 17:59:32 +0100

    Initial project

---
+++ src/main/java/app/arxivorg/ArxivOrgCLI.java
@@ -0,0 +1,9 @@
+package app.arxivorg;
+
+public class ArxivOrgCLI {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the arXiv organizer!");
+
+        System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
+    }
+}
