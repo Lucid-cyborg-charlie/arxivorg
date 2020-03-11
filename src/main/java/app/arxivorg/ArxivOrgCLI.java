72c0eba55797841cc180e6b00dbeca8114ac3824
Jeremy Auguste <jeremy.auguste@lis-lab.fr>
2020-03-11 16:48:16 +0100

    Une légère modification pour permettre la lecture de l'entrée standard

--- src/main/java/app/arxivorg/ArxivOrgCLI.java
+++ src/main/java/app/arxivorg/ArxivOrgCLI.java
@@ -1,9 +1,16 @@
 package app.arxivorg;
 
+import java.util.Scanner;
+
 public class ArxivOrgCLI {
     public static void main(String[] args) {
         System.out.println("Welcome to the arXiv organizer!");
 
         System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
+
+        System.out.println("Input your command: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sorry, I can't do anything yet ! (Read: " + scanner.nextLine() +")");
+        scanner.close();
     }
 }
