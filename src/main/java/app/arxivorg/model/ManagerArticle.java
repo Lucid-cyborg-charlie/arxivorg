b3a453d33e494717743c76ab08ef6c1ce1197132
SADOUN Djedjiga <djedjiga.SADOUN@etu.amu-univ.fr>
2020-03-16 17:15:05 +0100

    implementation des fonctionnalitées de la classe ManagerArticle

--- src/main/java/app/arxivorg/model/ManagerArticle.java
+++ src/main/java/app/arxivorg/model/ManagerArticle.java
@@ -1,9 +1,6 @@
 package app.arxivorg.model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import com.sun.syndication.feed.synd.*;
 import com.sun.syndication.io.SyndFeedInput;
@@ -17,8 +14,10 @@ public class ManagerArticle {
     private List<Article> articles;
     private Set<Author> authors;
     private Set<Categorie> categories;
+    private List<String> periods;
 
     public ManagerArticle(){
+        this.periods = new ArrayList<>();
         this.articles = new ArrayList<>();
         this.authors = new HashSet<>();
         this.categories = new HashSet<>();
@@ -44,7 +43,11 @@ public class ManagerArticle {
                 article.setTitle(entry.getTitle());
                 article.setSummary(entry.getDescription().getValue());
                 article.setUpdated(entry.getUpdatedDate().toString());
-                article.setPublished(entry.getPublishedDate().toString());
+                Date date = entry.getPublishedDate();
+                String[] d = date.toString().split(" ");
+                article.setPublished(d[d.length-1]);
+                this.periods.add(d[d.length - 1]);
+                System.out.println(d[d.length - 1]);
 
                 // Get the authors
                 for(SyndPersonImpl author: (List<SyndPersonImpl>) entry.getAuthors()){
@@ -66,34 +69,63 @@ public class ManagerArticle {
      *
      * @return articles
      */
-    public List<Article> getArticles() { return articles; }
+    public List<Article> getArticles() { return articles;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public Set <Categorie> getCategories() {
+        return categories;
+    }
 
     /**
      *
      * @param categorie
      * @return  article by category list
      */
-    public List<Article> getArticlesByCategory(Categorie categorie){ return null; }
+    public List<Article> getArticlesByCategory(Categorie categorie){
+        List<Article> list = new ArrayList<>();
+        for(Article article : articles){
+            ArticleCategorie articlecategory = new ArticleCategorie(article,categorie);
+
+            if(article.getArticleCategories().contains(articlecategory)){
+                list.add(article);
+            }
+        }
+        return list;
+    }
 
     /**
      *
      * @param date
      * @return articles by periode
      */
-    public List<Article> getArticlesByPeriod(String date){ return null; }
-
-    /**
-     *
-     * @param keyWord
-     * @return article by keyword list
-     */
-    public List<Article> getArticlesByKeyWord(String keyWord){ return null; }
+    public List<Article> getArticlesByPeriod(String date){
+        List<Article> list=new ArrayList<>();
+        for(Article article: articles){
+            if(article.getPublished().equals(date)){
+                list.add(article);
+            }
+        }
+        return list;
+    }
 
     /**
      *
      * @param author
      * @return articles by author's name
      */
-    public List<Article> getArticlesByAuthor(Author author){ return null; }
+    public List<Article> getArticlesByAuthor(Author author){
+        List<Article> authors = new ArrayList<>();
+        for(Article article : articles){
+            ArticleAuthor articleauthor = new ArticleAuthor(article,author);
+            if(article.getArticleCategories().contains(articleauthor)){
+                authors.add(article);
+            }
+        }
+        return authors;
+    }
 
 }
